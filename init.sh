8268a5636c6164fc831f4d952b7b8ae56de7a848
Hong, Jian-Ching <allyusd@users.noreply.github.com>
2018-09-25 13:34:42 +0800

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,2 @@
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/allyusd/script/master/oh-my-zsh.sh)"
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/allyusd/script/master/docker.sh)"
