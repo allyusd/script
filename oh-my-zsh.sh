3ea197fc80812aac1fbbff1fcbae82a06cc42995
Hong, Jian-Ching <allyusd@users.noreply.github.com>
2018-09-25 14:30:25 +0800

    Update oh-my-zsh.sh

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -1,3 +1,4 @@
 sudo apt install -y zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sed -i s#robbyrussell#ys#g .zshrc
+sudo chsh -s /bin/zsh $USER
