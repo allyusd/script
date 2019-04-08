64cced2d7cd3c1a27a350117d22a73c735d39aa7
Hong, Jian-Ching <allyusd@users.noreply.github.com>
2019-04-08 17:41:50 +0800

    Update gitlab-runner.sh

--- gitlab-runner.sh
+++ gitlab-runner.sh
@@ -1,3 +1,3 @@
 curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh | sudo bash
 sudo apt-get install gitlab-runner
-gitlab-runner register
+sudo gitlab-runner register
