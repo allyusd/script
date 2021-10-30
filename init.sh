defe18d7e570642112b5e3713a35de063d09f570
Hong, Jian-Ching <allyusd@users.noreply.github.com>
2021-10-30 14:57:46 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get upgrade -y
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/allyusd/script/master/oh-my-zsh.sh)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/allyusd/script/master/docker.sh)"
