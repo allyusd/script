190d69a7b627d8d992161b662c8a157b3303c957
Hong, Jian-Ching <allyusd@users.noreply.github.com>
2021-10-30 14:55:15 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,3 +3,4 @@ sudo apt-get upgrade
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/allyusd/script/master/oh-my-zsh.sh)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/allyusd/script/master/docker.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/allyusd/script/master/docker-compose.sh)"
